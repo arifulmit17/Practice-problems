1e38bccc6d13173c3ec71d9c6acd17ff47d1f65d
Ariful <arifulmit17@gmail.com>
2025-11-13 11:49:44 +0600

    added generic func

---
+++ module-5/genericsfunction.ts
@@ -0,0 +1,11 @@
+const createArrayWithString=(value: string)=>[value]
+const createArrayWithNumber=(value: number)=>[value]
+const createArrayWithUserObj=(value: {id:number ; name:string})=>[value]
+const createArrayWithGeneric=<T>(value: T)=>[value]
+
+const createArrayWithGenericTuple=<X,Y>(param1:X,param2:Y)=>[param1,param2]
+
+const arrst=createArrayWithString('apple')
+const arrn=createArrayWithNumber(10)
+const arrobj=createArrayWithGeneric({id:1,name:"anb"})
+const arrt= createArrayWithGenericTuple('avf',1)
