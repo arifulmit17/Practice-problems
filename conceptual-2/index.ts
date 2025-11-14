d2e660359b22a369e6114777720e6873d1e06d34
Ariful <arifulmit17@gmail.com>
2025-11-14 11:42:56 +0600

    added comment

--- conceptual-2/index.ts
+++ conceptual-2/index.ts
@@ -2,3 +2,7 @@
  
 const number : Number=2
 console.log(number);
+
+// never is a type which is not going to finish or have an end point.
+
+// unknown type checks a type of a variable, but any disables type.
